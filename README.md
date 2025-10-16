01b12d4f128605197e30a8ff288d31f5c1abe4e5
Jean Salomon <jean2salomon@icloud.com>
2025-10-16 18:02:14 +0200

    updated the compile.sh for mac too

---
+++ README.md
@@ -0,0 +1,28 @@
+# algo tri
+
+
+## installation
+
+### requirements
+
+- SDL3
+- pkg-config (for the flags)
+
+```bash
+sudo apt install sdl3 pkg-config    # brew or pacman or smth else
+```
+
+### repo clone
+
+```bash
+git clone git@github.com:Skyleeze/Sort-Algorithm-Viewer.git
+```
+
+### compile
+
+give the execution permission and execute the file
+
+```bash
+chmod +x compile.sh
+./compile.sh
+```
--- compile.sh
+++ compile.sh
@@ -2,10 +2,20 @@
 
 # Pour lancer, utiliser sh compile.sh ou bash compile.sh ou ./compile.sh (nécessite autorisation (chmod))
 
+# stoppe le script en cas d'erreur
+set -e  
+
+# dossier build
 mkdir -p build
 
+# les fichiers sources
 SRC_FILES=$(find src/ -name '*.c')
 
-gcc $SRC_FILES -o ./build/application -lSDL3 -Iinclude
+# les flags
+CFLAGS=$(pkg-config --cflags sdl3)
+LDFLAGS=$(pkg-config --libs sdl3)
 
+# compilation
+gcc $SRC_FILES -o build/application $CFLAGS $LDFLAGS -Iinclude
 ./build/application
+
